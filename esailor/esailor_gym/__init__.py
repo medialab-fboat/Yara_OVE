60b59c4a2318047763e4d31e171998b00722bf5e
Eduardo Charles Vasconcellos <charles.edu@gmail.com>
2024-03-11 11:01:47 -0300

    Gymnasium env Eboat53-v0 was builded and tested. Envs Eboat925-v0, Eboat92-v0, and Eboat62-v0 were removed. Working on env Eboat103-v0 that will implement obstacle avoidance.

--- esailor/esailor_gym/__init__.py
+++ esailor/esailor_gym/__init__.py
@@ -9,51 +9,13 @@ logger = logging.getLogger(__name__)
 
 #-> Eboat gymnasium_envs
 register(
-    id='EboatSingleWayPointEnvCC35-v0',
-    entry_point='esailor_gym.envs:EboatSingleWayPointEnvCC35v0',
+    id='Eboat53-v0',
+    entry_point='esailor_gym.envs:Eboat53_v0',
     max_episode_steps=1000000,
 )
 
 register(
-    id='EboatSingleWayPointEnvCC25-v0',
-    entry_point='esailor_gym.envs:EboatSingleWayPointEnvCC25v0',
-    max_episode_steps=1000000,
-)
-
-register(
-    id='EboatStraightLineEnvCC29-v0',
-    entry_point='esailor_gym.envs:EboatStraightLineEnvCC29v0',
-    max_episode_steps=1000000,
-)
-
-register(
-    id='Eboat92_5-v0',
-    entry_point='esailor_gym.envs:Eboat925_v0',
-    max_episode_steps=1000000,
-)
-
-register(
-    id='Eboat92-v0',
-    entry_point='esailor_gym.envs:Eboat92_v0',
-    max_episode_steps=1000000,
-)
-
-register(
-    id='Eboat62-v0',
-    entry_point='esailor_gym.envs:Eboat62_v0',
-    max_episode_steps=1000000,
-)
-
-
-register(
-    id='Eboat93-v0',
-    entry_point='esailor_gym.envs:Eboat93_v0',
-    max_episode_steps=1000000,
-)
-
-
-register(
-    id='Eboat93-v1',
-    entry_point='esailor_gym.envs:Eboat93_v1',
+    id='Eboat103-v1',
+    entry_point='esailor_gym.envs:Eboat103_v0',
     max_episode_steps=1000000,
 )
